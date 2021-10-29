6d8c3e02a54ea0603d846a5f31685b812918a152
Jerrell Prier <jerrellprier@gmail.com>
2021-10-29 16:51:32 -0500

    Completed part 2 for about.js file

--- about.js
+++ about.js
@@ -5,10 +5,17 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	//console.log('form submit');
+	alert("The form has been submitted successfully!");
+}
+
+const handleMouseover = evt =>{
+	alert("U's is smarts");
 }
 
 
 let form = document.querySelector('form#contact');
+let img = document.querySelector('img');
 
 form.addEventListener('submit', handleSubmit);
+img.addEventListener('mouseover',handleMouseover);
