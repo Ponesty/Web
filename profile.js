9a517780b3f8f4d47796afd0cd2f317620ab1991
Jerrell Prier <jerrellprier@gmail.com>
2021-10-30 22:50:13 -0500

    Worked in styles.css, profile.html, and profile.js for Part 5. Requirements completed for part 5.

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
-const colorAlert = evt => alert("Blue");
-const placeAlert = evt => alert("Japan");
-const ritualAlert = evt => alert("PC Gaming");
+const colorAlert = evt => alert("Blue!");
+const placeAlert = evt => alert("Japan!");
+const ritualAlert = evt => alert("PC Gaming!");
 
 let colorBtn = document.querySelector('button#color');
 let placeBtn = document.querySelector('button#place');
